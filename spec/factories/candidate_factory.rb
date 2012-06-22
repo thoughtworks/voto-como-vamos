d0bd767d3c7c8a6371531171d2a4be9fdc453d0f
Bruno Pinto <bpinto@thoughtworks.com>
2012-06-22 00:46:42 -0300

    candidate factory defines all its fields

--- spec/factories/candidate_factory.rb
+++ spec/factories/candidate_factory.rb
@@ -1,5 +1,16 @@
 FactoryGirl.define do
   factory :candidate do
-    name 'Candidato Fulano de Tal'
+    name       'Candidato Fulano de Tal'
+    role       'F_ROLE'
+    party      'F_PARTY'
+    alliance   'F_ALLIANCE'
+    tse_number 'F_TSE_NUMBER'
+    email      'F_EMAIL'
+    about      'F_ABOUT'
+    phone      'F_HONE'
+    site       'F_SITE'
+    blog       'F_BLOG'
+    facebook   'F_FACEBOOK'
+    twitter    'F_TWITTER'
   end
 end
