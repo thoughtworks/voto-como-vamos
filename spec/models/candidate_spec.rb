29a76d66fb388ea773b774d959977e01fba40edc
Jefferson Girao <contato@jefferson.eti.br>
2012-06-26 21:18:42 -0300

    Removing duplicated specs

--- spec/models/candidate_spec.rb
+++ spec/models/candidate_spec.rb
@@ -11,10 +11,6 @@ describe Candidate do
     it { should ensure_length_of(:about).is_at_most(500) }
     it { should ensure_length_of(:phone).is_equal_to(13) }
        
-    it { should validate_presence_of :email }
-    it { should validate_format_of(:email).with("valid@email.com") }
-    it { should validate_format_of(:email).not_with("invalid.com") }
-    
     it { should validate_format_of(:site).not_with("invalid_url") }
     it { should validate_format_of(:blog).not_with("invalid_url") }
     it { should validate_format_of(:facebook).not_with("invalid_url") }
