088c1cd54b019253eeb774b1bf32495c45cd0de2
Jefferson Girao <contato@jefferson.eti.br>
2012-07-07 19:31:48 -0300

    Code gardening

--- app/models/proposal.rb
+++ app/models/proposal.rb
@@ -8,11 +8,13 @@ class Proposal < ActiveRecord::Base
   belongs_to :candidate
   validates :candidate, :presence => true
 
-  validates :title, :presence => true,
-                    :length => { :maximum => 100 }
+  validates :title, 
+    :presence => true,
+    :length => { :maximum => 100 }
 
-  validates :abstract, :presence => true,
-                       :length => { :maximum => 300 }
+  validates :abstract, 
+    :presence => true,
+    :length => { :maximum => 300 }
 
   validates :description, :presence => true
 
