f8e06948cd029034abf7c7788f89f578cb1dcb98
cribeiro <Thoughtworks@TW008317.local>
2012-07-07 22:51:11 -0300

    refactoring facotries to cover new cornecer cases scenarios

--- spec/factories/proposal_factory.rb
+++ spec/factories/proposal_factory.rb
@@ -8,9 +8,31 @@ FactoryGirl.define do
     candidate
   end
 
-  factory :invalid_proposal, :class => Proposal do
+  factory :invalid_proposal_missing_all_fields, :class => Proposal do
     title       ''
     abstract    ''
     description ''
+    candidate
+  end
+
+  factory :invalid_proposal_missing_description, :class => Proposal do
+    title       'Título'
+    abstract    'Resumo'
+    description ''
+    candidate
+  end
+
+  factory :invalid_proposal_missing_title, :class => Proposal do
+    title       ''
+    abstract    'Resumo'
+    description 'Descrição'
+    candidate
+  end
+  
+  factory :invalid_proposal_missing_abstract, :class => Proposal do
+    title       'Título'
+    abstract    ''
+    description 'Descrição'
+    candidate
   end
 end
