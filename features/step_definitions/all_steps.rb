111cb7f949b52713e35699205354302fc55dee6b
Fernando Muraro Witzke <fwitzke@thoughtworks.com>
2012-07-14 16:55:16 -0300

    #7 showing 'nome de urna' instead of full name

--- features/step_definitions/all_steps.rb
+++ features/step_definitions/all_steps.rb
@@ -157,7 +157,7 @@ end
 
 Entao /^eu devo ver as suas informações$/ do
   [
-    :alliance, :about, :email, :name, :party, :phone, :role, :tse_number
+    :alliance, :about, :email, :short_name, :party, :phone, :role, :tse_number
   ].each do |field|
     page.should have_content(@candidate.send(field))
   end
