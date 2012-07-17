25f0cc3e851e614f910ce751101dc9d2e4069d15
Bruno Tavares <bruno.exz@gmail.com>
2012-07-17 20:38:53 -0300

    Traduzindo as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,18 @@ VotoComoVamos::Application.routes.draw do
   match '/auth/facebook/callback' => 'sessions#create'
   match '/logout' => 'sessions#destroy'
 
-  resources :candidates do
-    resources :proposals, :only => [:new, :create, :show, :destroy, :edit, :update] do
-      get :delete
+  scope :scope_paths => { :new => "novo", :edit => "editar" } do
+    resources :candidates, :path => "candidatos" do
+      resources :proposals, :path => "propostas", :only => [:new, :create, :show, :destroy, :edit, :update] do
+        get :delete
+      end
+      resources :ownerships
+      resources :revindications, :shallow => true,
+        :only => [:new, :create, :show]
     end
-    resources :ownerships
-    resources :revindications, :shallow => true,
-      :only => [:new, :create, :show]
+
+    resources :proposals, :path => "propostas"
+    resources :opinions, :path => "opinioes"
   end
-
-  resources :proposals
-  resources :opinions
-
   root :to => 'welcome#index'
 end
