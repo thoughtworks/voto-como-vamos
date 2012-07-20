a4f48226a2392a9c4ace294805629ebcf288f575
Alexandre C. Barbosa <alexandre.correa@gmail.com>
2012-07-20 20:30:47 -0300

    Removing FacebookRequestTranslator middleware

--- config/application.rb
+++ config/application.rb
@@ -58,7 +58,5 @@ module VotoComoVamos
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-
-    config.middleware.use "Rack::FacebookRequestTranslator"
   end
 end
