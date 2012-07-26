065a94465bf7787731f5cb22036d2f6b26b629c1
Jefferson Girao <contato@jefferson.eti.br>
2012-07-25 23:04:21 -0300

    Fixing hostname for e-mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,9 @@ VotoComoVamos::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.default_url_options = {
+    :host => "teste.votocomovamos.com.br"
+  }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address              => "smtp.gmail.com",  
