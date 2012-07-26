c0bc3f289110ce877c32ea5e0405227a19d63296
Fernando Freire <thenano@gmail.com>
2012-07-25 22:07:31 -0300

    +bruno mailer configuration.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,14 @@ VotoComoVamos::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",  
+    :port                 => 587,                 
+    :domain               => 'votocomovamos.com.br',  
+    :user_name            => ENV['mailer_user'],      
+    :password             => ENV['mailer_pass'],      
+    :authentication       => 'plain',             
+    :enable_starttls_auto => true
+  }  
 end
