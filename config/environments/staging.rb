ffba416215ad348c469811eff638bb59e8c231e7
Fernando Freire <thenano@gmail.com>
2012-07-30 23:14:52 -0300

    disabling questions and updating revindicaition email

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -64,6 +64,7 @@ VotoComoVamos::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  config.action_mailer.asset_host = "http://teste.votocomovamos.com.br"
   config.action_mailer.default_url_options = {
     :host => "teste.votocomovamos.com.br"
   }
