2cf81ae1d765983fcef63c899e0ad5af7edd951c
Jefferson Girao <jgirao@thoughtworks.com>
2012-09-18 22:36:37 -0300

    Basic E-mail notification (wip)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :proposal
   belongs_to :user
+  after_commit :notify_candidate, :on => :create
+
+  def notify_candidate
+    CandidateMailer.notify_comment(proposal.id).deliver
+  end
 end
